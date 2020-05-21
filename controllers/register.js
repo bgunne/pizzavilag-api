6a4724a03b183f4d6585cc2bd22a8c946cfa61e7
Tamas Ujj <bgunne@gmail.com>
2020-05-21 12:37:10 +0200

    fix

--- controllers/register.js
+++ controllers/register.js
@@ -1,43 +1,45 @@
-const  handleRegister = async(req,res,db, bcrypt) =>
-{
-    const {email, firstname, lastname, phone, zip, city, address, comment, password} = req.body;
+const handleRegister = async (req, res, db, bcrypt) => {
+    const { email, firstname, lastname, phone, zip, city, address, comment, password } = req.body;
 
-    if(!email || !firstname || !lastname || !phone || !zip || !city || !address || !password)
-    {
-        
+    if (!email || !firstname || !lastname || !phone || !zip || !city || !address || !password) {
+
         return res.status(400).json('Töltsd ki a kötelező mezőket!');
 
     }
     const hash = bcrypt.hashSync(password);
 
-    db.transaction( async function (trx){
-        const loginEmail = trx.insert(
+    db.transaction(trx => {
+        trx.insert(
             {
                 hash: hash,
                 email: email
             }
         )
-        .into('login')
-        .returning('email');
-        const user = await trx('users')
-            .returning('*')
-            .insert(
-            {
-                email: loginEmail[0],
-                firstname: firstname,
-                lastname: lastname,
-                phone: phone,
-                zip: zip,
-                city: city,
-                address: address,
-                comment: comment,
-                joined: new Date()
-            });
-        
-        await res.json(user[0]);
-        
-        return trx.commit;
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert(
+                        {
+                            email: loginEmail[0],
+                            firstname: firstname,
+                            lastname: lastname,
+                            phone: phone,
+                            zip: zip,
+                            city: city,
+                            address: address,
+                            comment: comment,
+                            joined: new Date()
+                        })
+                    .then(user => {
+                        res.json(user[0]);
+                    })
+            })
+            .then(trx.commit)
+            .catch(trx.rollback);
     })
+        .catch(err => res.status(400).json(err));
 }
 
 
