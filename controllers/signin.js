977e86673abd97203288d0e43c4d847d5adfd83d
Tamas Ujj <bgunne@gmail.com>
2020-05-21 12:32:06 +0200

    fix

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,18 +1,17 @@
-async function handleSignin(req,res,db,bcrypt){
+const handleSignin = async (req, res, db, bcrypt) => {
 
-    const {email, password} = req.body;
-    if(!email || !password)
-    {
+    const { email, password } = req.body;
+    if (!email || !password) {
         return res.status(400).json('Minden mező kitöltése kötelező!');
     }
 
-    const data = await db.select('email', 'hash').from('login').where('email','=',email);
+    const data = await db.select('email', 'hash').from('login').where('email', '=', email);
     const isValid = bcrypt.compareSync(password, data[0].hash);
-    if(isValid){
-        const user = await db.select('*').from('users').where('email','=',email);
+    if (isValid) {
+        const user = await db.select('*').from('users').where('email', '=', email);
         return res.json(user[0]);
     }
-    else{
+    else {
         return res.status(400).json('Felhasználónév vagy jelszó hibás.');
     }
 }
